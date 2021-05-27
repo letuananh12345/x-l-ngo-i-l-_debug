e06d004e94bb6b8e5e3ea4efb41f11d558bd8ae7
letuananh12345 <letuananhmdc6789@gmail.com>
2021-05-27 10:18:52 +0700

    thuc hanh 1

---
+++ src/thuc_hanh_1/ArrayExample.java
@@ -0,0 +1,32 @@
+package thuc_hanh_1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom(){
+        Random rd=new Random();
+        Integer[] arr=new Integer[100];
+        System.out.println("Danh sách phần tử của mảng: ");
+        for(int i=0; i<100;i++){
+            arr[i]=rd.nextInt(100);
+            System.out.println(arr[i]+ " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample=new ArrayExample();
+        Integer[] arr= arrayExample.createRandom();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n nhập chỉ số của 1 phần tử bất kỳ : ");
+        int x = scanner.nextInt();
+        try{
+            System.out.println("giá trị phần tử có chỉ số 5 "+x+"là"+ arr[x]);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("chỉ số vượt qua giới hạn của mảng");
+        }
+
+    }
+}
+
